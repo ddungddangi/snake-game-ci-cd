e9420ecc7b7c178161cc272da0f4db982d2dd807
ddungddangi <gaemineunttungttung@gmail.com>
2025-03-26 16:00:48 +0900

    add a new workflow22

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def update_high_score():
 # ==================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
